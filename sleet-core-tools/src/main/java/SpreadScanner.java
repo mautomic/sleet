c46a2b5b790b03d5753a58062c3dbd27f70061f0
maurya373 <maurya373@gmail.com>
2019-08-03 11:23:50 -0400

    add multithreading for more than one ticker

--- sleet-core-tools/src/main/java/SpreadScanner.java
+++ sleet-core-tools/src/main/java/SpreadScanner.java
@@ -1,25 +1,13 @@
 package com.sleet.tools;
 
-import com.sleet.api.domain.Option;
-import com.sleet.api.domain.OptionChain;
 import com.sleet.api.service.OptionService;
-import com.sleet.tools.objects.Contract;
-import com.sleet.tools.objects.Spread;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-
-import java.math.BigDecimal;
-import java.math.RoundingMode;
-import java.util.*;
-import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.TimeUnit;
 
 public class SpreadScanner {
 
     private static final Logger LOG = LogManager.getLogger(SpreadScanner.class);
-    private static final int API_REQUEST_INTERVAL = 60;
-    private static final int DELAY = 2;
+    private static final String[] WATCHLIST = {"SPX", "SPY", "QQQ", "VIX", "TLT"};
 
     public static void main(String[]args) {
 
@@ -28,87 +16,10 @@ public class SpreadScanner {
         LOG.info("Retrieving API key");
         OptionService optionService = new OptionService(GlobalProperties.getInstance().getApiKey());
 
-        Runnable retrieveSpreads = () -> {
+        for(String ticker : WATCHLIST) {
 
-            OptionChain optionChain = optionService.getCloseExpirationOptionChain("SPX", "2019-09-21");
-            Map<String, Map<String, List<Option>>> callMap = optionChain.getCallExpDateMap();
-            Map<String, Map<String, List<Option>>> putMap = optionChain.getPutExpDateMap();
-
-            List<Spread> callSpreadList = getSpreadList(callMap, Contract.CALL.name());
-            List<Spread> putSpreadList = getSpreadList(putMap, Contract.PUT.name());
-
-            Collections.sort(putSpreadList);
-
-            for(Spread spread : putSpreadList)
-                if (spread.getBuyingPower() <= 5000 && spread.getRoi() >= 0.05)
-                    LOG.info(spread.toString());
-        };
-
-        LOG.info("Starting scheduled executor");
-        ScheduledExecutorService executor = Executors.newScheduledThreadPool(1);
-        executor.scheduleAtFixedRate(retrieveSpreads, DELAY, API_REQUEST_INTERVAL, TimeUnit.SECONDS);
-    }
-
-    private static List<Spread> getSpreadList(Map<String, Map<String, List<Option>>> optionMap, String type) {
-
-        Set<String> dates = optionMap.keySet();
-        List<String> strikes;
-
-        List<Spread> spreads = new ArrayList<>();
-
-        for (String date : dates) {
-
-            if(date.split(":")[1].equalsIgnoreCase("0"))
-                continue;
-
-            strikes = new ArrayList<>(optionMap.get(date).keySet());
-
-            // Only OTM options
-            if (type.equalsIgnoreCase(Contract.CALL.name()))
-                strikes = strikes.subList(strikes.size()/2, strikes.size());
-            else
-                strikes = strikes.subList(0, strikes.size()/2);
-
-            for (int i = 0; i < strikes.size() - 1; i++) {
-                for (int j = i + 1; j < strikes.size(); j++) {
-
-                    Option shortLeg;
-                    Option longLeg;
-
-                    try {
-
-                        if (type.equalsIgnoreCase(Contract.CALL.name())) {
-                            shortLeg = optionMap.get(date).get(strikes.get(i)).get(0);
-                            longLeg = optionMap.get(date).get(strikes.get(j)).get(0);
-                        } else {
-                            shortLeg = optionMap.get(date).get(strikes.get(j)).get(0);
-                            longLeg = optionMap.get(date).get(strikes.get(i)).get(0);
-                        }
-
-                        String spreadName = shortLeg.getStrikePrice() + "/" + longLeg.getStrikePrice();
-                        int expirationDate = Integer.parseInt(date.substring(date.indexOf(":")+1));
-
-                        double spreadPrice = shortLeg.getMark() - longLeg.getMark();
-                        BigDecimal roundedSpreadPrice = new BigDecimal(spreadPrice).setScale(2, RoundingMode.HALF_UP);
-                        BigDecimal multiplier = new BigDecimal(100);
-
-                        BigDecimal roundedBuyingPower = new BigDecimal(shortLeg.getStrikePrice() - longLeg.getStrikePrice());
-                        roundedBuyingPower = roundedBuyingPower.multiply(multiplier).abs().setScale(2, RoundingMode.HALF_UP);
-
-                        BigDecimal roundedRoi = roundedSpreadPrice.multiply(multiplier).divide(roundedBuyingPower, 3, RoundingMode.HALF_UP);
-
-                        Spread spread = new Spread(spreadName, expirationDate, roundedSpreadPrice.doubleValue(),
-                                roundedBuyingPower.intValue(), roundedRoi.doubleValue());
-
-                        spreads.add(spread);
-
-                    } catch(Exception e) {
-                        LOG.error(e.getMessage());
-                    }
-                }
-            }
+            SpreadRetrieverTask task = new SpreadRetrieverTask(optionService, ticker);
+            new Thread(task).start();
         }
-
-        return spreads;
     }
 }
--- sleet-core-tools/src/main/java/objects/Spread.java
+++ sleet-core-tools/src/main/java/objects/Spread.java
@@ -2,20 +2,24 @@ package com.sleet.tools.objects;
 
 public class Spread implements Comparable<Spread> {
 
+    private String ticker;
     private String spread;
     private int expirationDays;
     private double price;
     private double buyingPower;
     private double roi;
 
-    public Spread(String spread, int expirationDays, double price, double buyingPower, double roi) {
+    public Spread(String ticker, String spread, int expirationDays, double price, double buyingPower, double roi) {
 
+        this.ticker = ticker;
         this.spread = spread;
         this.expirationDays = expirationDays;
         this.price = price;
         this.buyingPower = buyingPower;
         this.roi = roi;
     }
+
+    public String getTicker() { return ticker; }
 
     public String getSpread() {
         return spread;
@@ -39,7 +43,7 @@ public class Spread implements Comparable<Spread> {
 
     @Override
     public String toString() {
-        return "[ " + spread + " " + expirationDays + " " + price + " " + buyingPower + " " + roi + " ]";
+        return "[ " + ticker + " " + spread + " " + expirationDays + " " + price + " " + buyingPower + " " + roi + " ]";
     }
 
     @Override
