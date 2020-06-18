9f2b6bc0744d9a61d7027fec6263a90fa76f2a2c
Victor Philip <vphilipnyc@gmail.com>
2020-06-18 15:50:10 -0400

    adds a method to get multiple quotes; uses Optional instead of returning null
    Depending on usage, it may be a good idea to cache the URL instead of creating it each time the method is called.  Might be some ways to optimize it.

--- sleet-api-service/src/main/java/com/sleet/api/service/QuoteService.java
+++ sleet-api-service/src/main/java/com/sleet/api/service/QuoteService.java
@@ -2,16 +2,17 @@ package com.sleet.api.service;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.sleet.api.model.Equity;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import com.sleet.api.model.Equity;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
 
+import java.net.URL;
+import java.util.*;
+
 /**
  * A {@link Service} implementation that provides methods to retrieve equity quote data from the TD API
- *
- * TODO: Implement multiple quotes query
  */
 public class QuoteService extends Service {
 
@@ -30,15 +31,39 @@ public class QuoteService extends Service {
      * @param ticker to get quote info for
      * @return an {@link Equity} with quote information
      */
-    public Equity getQuote(final String ticker) {
+    public Optional<Equity> getQuote(final String ticker) {
 
         final String url = API_URL + ticker + QUOTE_URL;
         try {
             final ResponseEntity<JsonNode> json = restTemplate.getForEntity(url, JsonNode.class);
-            return mapper.readValue(json.getBody().get(ticker).toString(), Equity.class);
-        } catch(Exception e) {
-            LOG.error("Could not retrieve quote info for " + ticker, e);
+            Equity equity = mapper.readValue(Objects.requireNonNull(json.getBody())
+                    .get(ticker).toString(), Equity.class);
+            return Optional.ofNullable(equity);
+        } catch (Exception e) {
+            LOG.error("Could not retrieve quote for " + ticker, e);
         }
-        return null;
+        return Optional.empty();
+    }
+
+    public Optional<List<Equity>> getQuotes(final String... tickers) {
+
+        String concatenated = String.join("%2C", tickers);
+        final String url = API_URL + QUOTE_URL + "&symbol=" + concatenated;
+
+        try {
+            JsonNode node = mapper.readTree(new URL(url));
+            List<Equity> equities = new ArrayList<>(tickers.length);
+
+            for (String ticker : tickers) {
+                JsonNode topLevel = node.path(ticker);
+                Equity equity = mapper.treeToValue(topLevel, Equity.class);
+                equities.add(equity);
+            }
+            return Optional.of(equities);
+
+        } catch (Exception e) {
+            LOG.error("Could not retrieve quote for " + Arrays.toString(tickers), e);
+        }
+        return Optional.empty();
     }
 }
