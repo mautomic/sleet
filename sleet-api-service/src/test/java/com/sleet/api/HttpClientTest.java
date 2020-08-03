e35ed9fab65e15da3f8f841a8cfdeeeefd9330a6
mautomic <maurya373@gmail.com>
2020-08-02 19:36:18 -0500

    add tests for HttpClient POST

--- sleet-api-service/src/test/java/com/sleet/api/HttpClientTest.java
+++ sleet-api-service/src/test/java/com/sleet/api/HttpClientTest.java
@@ -2,6 +2,7 @@ package com.sleet.api;
 
 import org.asynchttpclient.Response;
 import org.junit.Assert;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 import java.util.concurrent.CompletableFuture;
@@ -14,11 +15,16 @@ import java.util.concurrent.TimeUnit;
  */
 public class HttpClientTest {
 
+    static HttpClient client;
+
+    @BeforeClass
+    public static void setup() {
+        client = new HttpClient(5000, 5000);
+    }
+
     @Test
     public void testGetSync() throws Exception {
         String url = "https://httpbin.org/get";
-        HttpClient client = new HttpClient(5000, 5000);
-
         Response response = client.get(url, 2000);
         Assert.assertEquals(200, response.getStatusCode());
         Assert.assertTrue(response.getResponseBody().length() > 0);
@@ -28,8 +34,6 @@ public class HttpClientTest {
     @Test
     public void testGetAsync() throws Exception {
         String url = "https://httpbin.org/get";
-        HttpClient client = new HttpClient(5000, 5000);
-
         CompletableFuture<Response> responseFuture = client.get(url);
         Assert.assertNotNull(responseFuture);
 
@@ -41,13 +45,43 @@ public class HttpClientTest {
     @Test
     public void testGetAsyncWithHandler() {
         String url = "https://httpbin.org/get";
-        HttpClient client = new HttpClient(5000, 5000);
-
         Handler<Response> handler = response -> {
             Assert.assertTrue(response.getResponseBody().length() > 0);
             Assert.assertTrue(response.getResponseBody().contains("httpbin.org"));
         };
-
         client.get(url, handler);
+    }
+
+    @Test
+    public void testPostSync() throws Exception {
+        String url = "https://httpbin.org/post";
+        String body = "key=value";
+        Response response = client.post(url, body, 2000);
+        Assert.assertEquals(200, response.getStatusCode());
+        Assert.assertTrue(response.getResponseBody().length() > 0);
+        Assert.assertTrue(response.getResponseBody().contains("httpbin.org"));
+    }
+
+    @Test
+    public void testPostAsync() throws Exception {
+        String url = "https://httpbin.org/post";
+        String body = "key=value";
+        CompletableFuture<Response> responseFuture = client.post(url, body);
+        Assert.assertNotNull(responseFuture);
+
+        Response response = responseFuture.get(10, TimeUnit.SECONDS);
+        Assert.assertTrue(response.getResponseBody().length() > 0);
+        Assert.assertTrue(response.getResponseBody().contains("httpbin.org"));
+    }
+
+    @Test
+    public void testPostAsyncWithHandler() {
+        String url = "https://httpbin.org/post";
+        String body = "key=value";
+        Handler<Response> handler = response -> {
+            Assert.assertTrue(response.getResponseBody().length() > 0);
+            Assert.assertTrue(response.getResponseBody().contains("httpbin.org"));
+        };
+        client.post(url, body, handler);
     }
 }
