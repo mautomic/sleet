a50fe186f2ce6dff5f1021386263e42aa1896847
Victor Philip <vphilipnyc@gmail.com>
2020-09-23 19:50:22 -0400

    no wildcards for imports; throws Exceptions instead of logging

--- sleet-api-service/src/main/java/com/sleet/api/service/QuoteService.java
+++ sleet-api-service/src/main/java/com/sleet/api/service/QuoteService.java
@@ -2,14 +2,15 @@ package com.sleet.api.service;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import com.sleet.api.model.Equity;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
 
 import java.net.URL;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
 
 /**
  * A {@link Service} implementation that provides methods to retrieve equity quote data from the TD API
@@ -17,7 +18,6 @@ import java.util.*;
 public class QuoteService extends Service {
 
     private static String QUOTE_URL;
-    private static final Logger LOG = LoggerFactory.getLogger(OptionService.class);
     private static final ObjectMapper mapper = new ObjectMapper();
 
     public QuoteService(final String apiKey) {
@@ -31,18 +31,13 @@ public class QuoteService extends Service {
      * @param ticker to get quote info for
      * @return an {@link Equity} with quote information
      */
-    public Optional<Equity> getQuote(final String ticker) {
+    public Optional<Equity> getQuote(final String ticker) throws Exception {
 
         final String url = API_URL + ticker + QUOTE_URL;
-        try {
-            final ResponseEntity<JsonNode> json = restTemplate.getForEntity(url, JsonNode.class);
-            Equity equity = mapper.readValue(Objects.requireNonNull(json.getBody())
-                    .get(ticker).toString(), Equity.class);
-            return Optional.ofNullable(equity);
-        } catch (Exception e) {
-            LOG.error("Could not retrieve quote for " + ticker, e);
-        }
-        return Optional.empty();
+        final ResponseEntity<JsonNode> json = restTemplate.getForEntity(url, JsonNode.class);
+        Equity equity = mapper.readValue(Objects.requireNonNull(json.getBody())
+                .get(ticker).toString(), Equity.class);
+        return Optional.ofNullable(equity);
     }
 
     /**
@@ -51,25 +46,19 @@ public class QuoteService extends Service {
      * @param tickers to get quote info for
      * @return an Optional List of {@link Equity} objects with quote information
      */
-    public List<Equity> getQuotes(final String... tickers) {
+    public List<Equity> getQuotes(final String... tickers) throws Exception {
 
         String concatenated = String.join("%2C", tickers);
         final String url = API_URL + QUOTE_URL + "&symbol=" + concatenated;
 
-        try {
-            JsonNode node = mapper.readTree(new URL(url));
-            List<Equity> equities = new ArrayList<>(tickers.length);
+        JsonNode node = mapper.readTree(new URL(url));
+        List<Equity> equities = new ArrayList<>(tickers.length);
 
-            for (String ticker : tickers) {
-                JsonNode topLevel = node.path(ticker);
-                Equity equity = mapper.treeToValue(topLevel, Equity.class);
-                equities.add(equity);
-            }
-            return equities;
-
-        } catch (Exception e) {
-            LOG.error("Could not retrieve quote for " + Arrays.toString(tickers), e);
+        for (String ticker : tickers) {
+            JsonNode topLevel = node.path(ticker);
+            Equity equity = mapper.treeToValue(topLevel, Equity.class);
+            equities.add(equity);
         }
-        return Collections.emptyList();
+        return equities;
     }
 }
