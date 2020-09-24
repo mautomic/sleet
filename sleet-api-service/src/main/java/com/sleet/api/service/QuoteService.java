b683da8e3c470f635664b3223f355848eabb25b7
Victor Philip <vphilipnyc@gmail.com>
2020-09-23 21:01:54 -0400

    updates method for multiple tickers

--- sleet-api-service/src/main/java/com/sleet/api/service/QuoteService.java
+++ sleet-api-service/src/main/java/com/sleet/api/service/QuoteService.java
@@ -1,27 +1,28 @@
 package com.sleet.api.service;
 
 import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
+import com.sleet.api.HttpClient;
 import com.sleet.api.model.Equity;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.client.RestTemplate;
+import org.asynchttpclient.Response;
 
-import java.net.URL;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
-import java.util.Objects;
 import java.util.Optional;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.TimeUnit;
 
 /**
  * A {@link Service} implementation that provides methods to retrieve equity quote data from the TD API
+ *
+ * @author mautomic
  */
 public class QuoteService extends Service {
 
     private static String QUOTE_URL;
-    private static final ObjectMapper mapper = new ObjectMapper();
 
     public QuoteService(final String apiKey) {
-        restTemplate = new RestTemplate(getClientHttpRequestFactory());
+        httpClient = new HttpClient(DEFAULT_TIMEOUT_MILLIS, DEFAULT_TIMEOUT_MILLIS);
         QUOTE_URL = "/quotes?apikey=" + apiKey;
     }
 
@@ -32,33 +33,44 @@ public class QuoteService extends Service {
      * @return an {@link Equity} with quote information
      */
     public Optional<Equity> getQuote(final String ticker) throws Exception {
+        final String url = API_URL + MARKETDATA + "/" + ticker + QUOTE_URL;
+        final CompletableFuture<Response> responseFuture = httpClient.get(url, null);
+        final Response response = responseFuture.get(DEFAULT_TIMEOUT_MILLIS, TimeUnit.MILLISECONDS);
 
-        final String url = API_URL + ticker + QUOTE_URL;
-        final ResponseEntity<JsonNode> json = restTemplate.getForEntity(url, JsonNode.class);
-        Equity equity = mapper.readValue(Objects.requireNonNull(json.getBody())
-                .get(ticker).toString(), Equity.class);
-        return Optional.ofNullable(equity);
+        if (response.getStatusCode() == 200) {
+            final String json = response.getResponseBody();
+            final JsonNode node = mapper.readValue(json, JsonNode.class);
+            return Optional.ofNullable(mapper.readValue(node.get(ticker).toString(), Equity.class));
+        }
+        return Optional.empty();
     }
 
     /**
      * Queries the TD API endpoint for current quote info for multiple tickers
      *
-     * @param tickers to get quote info for
-     * @return an Optional List of {@link Equity} objects with quote information
+     * @param tickers to get quotes for
+     * @return a list of {@link Equity} objects with quote information
      */
-    public List<Equity> getQuotes(final String... tickers) throws Exception {
-
+    public List<Equity> getQuotes(final List<String> tickers) throws Exception {
         String concatenated = String.join("%2C", tickers);
         final String url = API_URL + QUOTE_URL + "&symbol=" + concatenated;
+        final CompletableFuture<Response> responseFuture = httpClient.get(url, null);
+        final Response response = responseFuture.get(DEFAULT_TIMEOUT_MILLIS, TimeUnit.MILLISECONDS);
 
-        JsonNode node = mapper.readTree(new URL(url));
-        List<Equity> equities = new ArrayList<>(tickers.length);
+        if (response.getStatusCode() == 200) {
+            final String json = response.getResponseBody();
+            final JsonNode node = mapper.readValue(json, JsonNode.class);
 
-        for (String ticker : tickers) {
-            JsonNode topLevel = node.path(ticker);
-            Equity equity = mapper.treeToValue(topLevel, Equity.class);
-            equities.add(equity);
+            List<Equity> equities = new ArrayList<>(tickers.size());
+
+            for (String ticker : tickers) {
+                JsonNode topLevel = node.path(ticker);
+                Equity equity = mapper.treeToValue(topLevel, Equity.class);
+                equities.add(equity);
+            }
+            return equities;
         }
-        return equities;
+        return Collections.emptyList();
     }
 }
+
