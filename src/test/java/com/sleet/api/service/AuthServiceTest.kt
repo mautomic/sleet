1c7c148c310ce9fb21817eb8e3aa08f04cc50cac
mautomic <maurya373@gmail.com>
2021-04-11 17:50:10 -0500

    add td websocket streaming test

--- src/test/java/com/sleet/api/service/AuthServiceTest.kt
+++ src/test/java/com/sleet/api/service/AuthServiceTest.kt
@@ -1,11 +1,15 @@
 package com.sleet.api.service
 
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.sleet.api.ExampleClient
+import com.sleet.api.util.RequestUtil
 import org.asynchttpclient.Dsl
 import org.junit.Assert
 import org.junit.Ignore
 import org.junit.Test
 import kotlin.Throws
 import java.lang.Exception
+import java.net.URI
 import java.net.URLEncoder
 import java.nio.charset.StandardCharsets
 
@@ -64,5 +68,43 @@ class AuthServiceTest {
         Assert.assertNotNull(userPrincipals?.streamerSubscriptionKeys)
         Assert.assertNotNull(userPrincipals?.accounts)
         Assert.assertFalse(userPrincipals?.accounts!!.isEmpty())
+    }
+
+    @Test
+    @Ignore
+    @Throws(Exception::class)
+    fun testStreaming() {
+        val redirectUri = "https://127.0.0.1:8443/callback"
+        val authService = AuthService(TestConstants.API_KEY, redirectUri, Dsl.asyncHttpClient(Dsl.config()))
+
+        val userPrincipals = authService.getUserPrincipals(TestConstants.ACCESS_TOKEN)
+        val loginPayload = RequestUtil.createStreamingLoginPayload(userPrincipals!!)
+        println("Sent to ws: " + loginPayload)
+
+        val c = ExampleClient(URI("wss://streamer-ws.tdameritrade.com/ws"), loginPayload)
+        c.connect()
+
+        // Wait for a few seconds so we can connect before sending a quote request
+        Thread.sleep(3000)
+
+        val request = mapOf("service" to "QUOTE",
+            "requestid" to "service_count",
+            "command" to "SUBS",
+            "account" to TestConstants.ACCOUNT_NUM,
+            "source" to TestConstants.API_KEY,
+            "parameters" to mapOf<String, Any>(
+                "keys" to "AAPL",
+                "fields" to "0,1,2,3,4,5,6,7,8,9"
+            )
+        )
+
+        val requestString = ObjectMapper().writeValueAsString(request)
+        c.send(requestString)
+
+        var x = 0
+        while (x < 5) {
+            Thread.sleep(1000)
+            x++
+        }
     }
 }
