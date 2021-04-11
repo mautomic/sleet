a27244928368a780d567610ade77667261308944
mautomic <maurya373@gmail.com>
2021-04-11 14:29:46 -0500

    setup td streaming login payload creater

--- src/main/java/com/sleet/api/util/RequestUtil.kt
+++ src/main/java/com/sleet/api/util/RequestUtil.kt
@@ -1,7 +1,9 @@
 package com.sleet.api.util
 
+import com.sleet.api.model.UserPrincipals
 import org.asynchttpclient.Request
 import org.asynchttpclient.RequestBuilder
+import java.text.SimpleDateFormat
 
 /**
  * Utilities to create requests for a [AsyncHttpClient]
@@ -46,6 +48,23 @@ class RequestUtil {
                 requestBuilder.setHeader(name, value)
             }
             return requestBuilder.build()
+        }
+
+        @JvmStatic
+        fun createStreamingLoginPayload(userPrincipals: UserPrincipals): String {
+            val streamerInfo = userPrincipals.streamerInfo!!
+            val mainAccount = userPrincipals.accounts!![0]
+
+            val timestamp = streamerInfo.tokenTimestamp
+            val sdf = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss")
+            val epochMillis = sdf.parse(timestamp).time
+            val epochStr = epochMillis.toString()
+
+            return "{'requests':[{'service':'ADMIN','requestid':'0','command':'LOGIN','account':'${mainAccount.accountId}'," +
+                    "'source':'${streamerInfo.appId}','parameters':{'credential':'userid=${mainAccount.accountId}&token=${streamerInfo.token}" +
+                    "&company=${mainAccount.company}&segment=${mainAccount.segment}&cddomain=${mainAccount.accountCdDomainId}&usergroup=${streamerInfo.userGroup}" +
+                    "&accesslevel=${streamerInfo.accessLevel}&authorized=Y&timestamp=${epochStr}&appid=${streamerInfo.appId}&acl=${streamerInfo.acl}'," +
+                    "'token':'${streamerInfo.token}','version':'1.0'}}]}"
         }
     }
 }
